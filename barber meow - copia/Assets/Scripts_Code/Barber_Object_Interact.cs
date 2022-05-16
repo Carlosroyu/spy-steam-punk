5e9ca973454ebbc177ce40e28ea5919ecfecb14a
Carlosroyu <carlosroyu@gmail.com>
2022-05-16 11:14:41 +0200

    nuevos dialos UI y pruebas de codigo
    probando código para ver como tiene que funcionar mientras se ha añadido el dialogo final del tutorial y se ha cambiado parte de la ui

---
+++ barber meow - copia/Assets/Scripts_Code/Barber_Object_Interact.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barber_Object_Interact : MonoBehaviour
+{
+
+    public int barber_id = 0; 
+
+
+  private void FixedUpdate() {
+    Input.mouse
+  }
+  private void OnMouseOver()
+  {
+      
+      if(Input.GetMouseButtonDown(0))
+      {
+         //ConversationManager.Instance.SetInt("Barber_Item_ID", 1);
+      }
+
+  }
+}
--- barber meow - copia/Assets/Scripts_Code/OnClick_Start.cs
+++ barber meow - copia/Assets/Scripts_Code/OnClick_Start.cs
@@ -7,6 +7,16 @@ public class OnClick_Start : MonoBehaviour
 {
   public NPCConversation myConversation;
 
+  //Id de los item de la  barberia
+  const int Tijeras_id = 1;
+  const int Cuchilla_id = 2;
+  const int Peine_id = 3;
+  const int Secador_id = 4;
+  const int Champu_id = 5;
+  const int crema_afeitar_id = 6;
+  const int gomina_id = 7;
+
+
   private void OnMouseOver()
   {
       
@@ -14,6 +24,5 @@ public class OnClick_Start : MonoBehaviour
       {
           ConversationManager.Instance.StartConversation(myConversation);
       }
-
   }
 }
