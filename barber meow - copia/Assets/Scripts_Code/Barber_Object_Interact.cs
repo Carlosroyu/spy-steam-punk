ee08b514589418bf893e12cb658591544378e8be
Carlosroyu <carlosroyu@gmail.com>
2022-05-16 16:19:56 +0200

    mecanicas funcionales
    no se como mierdas lo hice pero funciona de milagro, mejor no tocarlo

--- barber meow - copia/Assets/Scripts_Code/Barber_Object_Interact.cs
+++ barber meow - copia/Assets/Scripts_Code/Barber_Object_Interact.cs
@@ -1,22 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DialogueEditor;
 
 public class Barber_Object_Interact : MonoBehaviour
 {
 
-    public int barber_id = 0; 
+    public int barber_id; 
 
-
-  private void FixedUpdate() {
-    Input.mouse
-  }
   private void OnMouseOver()
   {
-      
+
       if(Input.GetMouseButtonDown(0))
       {
-         //ConversationManager.Instance.SetInt("Barber_Item_ID", 1);
+         ConversationManager.Instance.SetInt("Barber_Item_ID", barber_id);
       }
 
   }
