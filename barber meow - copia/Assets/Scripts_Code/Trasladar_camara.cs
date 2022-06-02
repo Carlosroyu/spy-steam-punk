d0b5cca86bf9db6d256670ff5030e690df7624a8
Carlosroyu <carlosroyu@gmail.com>
2022-06-02 16:59:42 +0200

    Sistema de cámara y parallax
    ya funciona la cámara y el parallax para que cuando hable cada uno de los personajes la cámara se centre en uno de ellos

---
+++ barber meow - copia/Assets/Scripts_Code/Trasladar_camara.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+
+public class Trasladar_camara : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 gameCamera_Transition_Offset;
+    private float frame;
+    public float frameCounter;
+    public GameObject gameCamera;
+
+    private void Awake() {
+        gameCamera_Transition_Offset = new Vector3(2.5f,0,0);
+        frame = 0f;
+    }
+
+    // Update is called once per frame
+    public void MovegameCameraCat()
+    {
+        var targetPosition = gameCamera.transform.position + gameCamera_Transition_Offset;
+        var initPosition = gameCamera.transform.position;
+        frame = 0f;
+        StartCoroutine(MovegameCamera(initPosition, targetPosition));
+    }
+    public void MovegameCameraClient()
+    {
+        var targetPosition = gameCamera.transform.position - gameCamera_Transition_Offset;
+        var initPosition = gameCamera.transform.position;
+        frame = 0f;
+        StartCoroutine(MovegameCamera(initPosition, targetPosition));
+    }
+
+    private IEnumerator MovegameCamera(Vector3 initPosition, Vector3 targetPosition){
+        while(frame/frameCounter < 1){
+            gameCamera.transform.position = Vector3.Lerp(initPosition, targetPosition, frame/frameCounter);
+            frame++;
+
+            Debug.Log("La puta madre " + frame);
+            yield return frame;
+            yield return gameCamera;
+        }
+        yield return null;
+    }
+}
